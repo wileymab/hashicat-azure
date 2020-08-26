34138c3f7ebd178d7a81a049dd5bfda7898ad340
Matt Wiley <wileymab@gmail.com>
2020-08-26 18:34:39 +0000

    Correcting vnet config.

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,18 @@
+resource "azurerm_resource_group" "test" {
+  name     = "my-resources"
+  location = "West Europe"
+}
+
 module "network" {
   source  = "app.terraform.io/mbw-training/network/azurerm"
   version = "3.0.1"
-  # insert required variables here
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
 }
